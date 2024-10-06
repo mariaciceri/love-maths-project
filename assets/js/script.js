3dd4681ddce4bcdbe2cf58d00de451632e6a09d1
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-10-06 17:54:24 +0000

    create function names

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer(answer) {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplicationQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
