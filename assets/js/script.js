89900a87a09e7fc68ea574a97789cc51d14b6812
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-10-07 08:02:19 +0000

    add checkAnswer and calculateCorrectAnswer function logic. set feedback to the user when submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for(let button of buttons) {
         button.addEventListener("click", function() {
             if(this.getAttribute("data-type") === "submit") { //this. refer to the item in the array we are iterating through
-                alert("You clicked submit");
+                checkAnswer();
             }
             else {
                 let gameType = this.getAttribute("data-type");
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loaded
  * and after the users answer has been processed
  */
-
 function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 26) + 1;
     let num2 = Math.floor(Math.random() * 26) + 1;
@@ -39,12 +38,38 @@ function runGame(gameType) {
     }
 }
 
-function checkAnswer(answer) {
+/**
+ * Check the answer with the first element in the array returned from calculateCorrectAnswer
+ */
+function checkAnswer() {
+    let usersAnswer = parseInt(document.getElementById("answer-box").value); //must use value because is an input
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = usersAnswer === calculatedAnswer[0];
 
+    if(isCorrect) {
+        alert(`Hey! You got it right!`)
+    }
+    else {
+        alert(`Awww, not this time! You answered ${usersAnswer}. The correct value is ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);
 }
-
+ /**
+  * gets the operands and the operator directly from the dom, and return the correct answer.
+  */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText); // innerText retrieves the rendered text of an element, meaning it only returns what is actually visible to the user.
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if(operator === "+") {
+        return [operand1 + operand2, "add"]
+    }
+    else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
